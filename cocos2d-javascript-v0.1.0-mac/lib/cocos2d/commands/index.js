3ecc0863a4caeaf0858731972b40308986dabda0
Lennie Budgell <lbudgell@gmail.com>
2012-12-11 01:57:02 -0600

    Removed coco 2d library

--- cocos2d-javascript-v0.1.0-mac/lib/cocos2d/commands/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    'help':   require('./help'),
-    'new':    require('./new'),
-    'server': require('./server'),
-    'make':   require('./make'),
-    'ide':    require('./ide')
-};
